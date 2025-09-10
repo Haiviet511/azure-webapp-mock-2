2dff76f756a178d82ab0b7208a0ce8a98d8707e9
Haiviet511 <Haiviet411@gmail.com>
2025-09-10 13:34:48 +0700

    Update WebAppApplication.java

--- src/webapp/src/main/java/com/example/webapp/WebAppApplication.java
+++ src/webapp/src/main/java/com/example/webapp/WebAppApplication.java
@@ -2,8 +2,14 @@ package com.example.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.example.common.repository"})
+@EntityScan(basePackages = {"com.example.common.entity"})
+@ComponentScan(basePackages = {"com.example.webapp", "com.example.common"})
 public class WebAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebAppApplication.class, args);
